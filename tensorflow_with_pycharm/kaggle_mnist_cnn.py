faa8cd260eb81671737905aab055734ac6cbcd33
ChenXi <liucx.sky@gmail.com>
2018-09-03 14:36:01 +0800

    complete the temp_inputdata
    add regular
    add split
    add minbatch
    add get_test

--- tensorflow_with_pycharm/kaggle_mnist_cnn.py
+++ tensorflow_with_pycharm/kaggle_mnist_cnn.py
@@ -1,25 +1,25 @@
 import tensorflow as tf
 import temp_inputdata
 
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+mydata = temp_inputdata.MnistData()
 
 
 def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
 
 
 def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
 
 
 def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 
 def max_pool_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
 
 x = tf.placeholder("float", shape=[None, 784])
@@ -51,7 +51,7 @@ h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 # 全连接层
 W_fc2 = weight_variable([1024, 10])
 b_fc2 = bias_variable([10])
-y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
@@ -60,10 +60,12 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
+    batch = mydata.mini_batch(50)
+    test_data = mydata.get_test()
     for i in range(1000):
-        batch = mnist.train.next_batch(50)
+        next(batch)
         if i % 100 == 0:
             train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
             print("step %d, training accuracy %g" % (i, train_accuracy))
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-    print("test accuracy %g" % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+    print("test accuracy %g" % accuracy.eval(feed_dict={x: test_data[0], y_: test_data[1], keep_prob: 1.0}))
--- tensorflow_with_pycharm/temp_inputdata.py
+++ tensorflow_with_pycharm/temp_inputdata.py
@@ -1,39 +1,85 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 
 class MnistData(object):
     __data = np.loadtxt(r'C:\Users\Neo\ml_digit\data\train.csv', dtype="float32", delimiter=',', skiprows=1)
     __x = __data[:, 1:]
     __y = __data[:, 0]
-    m, n = __x.shape
 
-    x_tensor = __x
-    y_tensor = np.zeros((m, 10), dtype="float64")
-    count = 0
-    for i in __y:
-        y_tensor[count, int(i)] = 1.0
-        count += 1
+    """
+    官方处理的数据是形如这样的数据：
+    0.         0.5176471  0.6745098  0.         0.         0.
+    0.         0.         0.         0.         0.         0.
+    0.         0.         0.         0.         0.         0.
+    0.         0.         0.         0.         0.         0.32156864
+    0.63529414 0.7960785  0.7960785  0.5568628  0.2392157  0.20000002
+    0.98823535 0.32156864 0.         0.         0.         0.
+    0.         0.         0.         0.         0.         0.
+    0.         0.         0.         0.         0.         0.
+    0.         0.         0.08235294 0.9960785  0.9921569  0.9960785
+    按照常规的缩放方式，一定会把0给缩放了；但是看官方的数据格式，似乎只处理非0的值。
+    关于这一规则的进一步探索似乎只有看源码
+    方法中可以按照两个方式同时来做，一个是常规的把0也缩放，另外一个是按照官方提供的方式来做，看一下效果对比
+    官方代码是按照全部乘以1/255来进行的
+    """
 
+    # 对x做归一化处理, return x, y
+    def __regulate(self):
+        x = np.multiply(self.__x, 1.0/255.0)
+        y = self.__y
+        return x, y
+
+    # 按照8:2来拆分数据， return x_train, x_test, y_train, y_test
+    def __split(self):
+        size = 0.2
+        x, y = self.__regulate()
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=size, random_state=42)
+        return x_train, x_test, y_train, y_test
+
+    # 将y向量构造成y矩阵, test data可被外部访问 , return x_train_t, x_test_t, y_train_t, y_test_t
+    def __make_tensor(self, tot="train"):
+        x_train, x_test, y_train, y_test = self.__split()
+        x_train_t = x_train
+        x_test_t = x_test
+        y_train_t = np.zeros((y_train.shape[0], 10), dtype="float64")
+        y_test_t = np.zeros((y_test.shape[0], 10), dtype="float64")
+        count_train, count_test = 0, 0
+        for i in y_train:
+            y_train_t[count_train, int(i)] = 1.0
+            count_train += 1
+        for i in y_test:
+            y_test_t[count_test, int(i)] = 1.0
+            count_test += 1
+        if tot == "train":
+            return x_train_t, y_train_t
+        elif tot == "test":
+            return x_test_t, y_test_t
+
+    # use x_train_t, y_train_t, 每次大循环开始没有随机x和y
     def mini_batch(self, num):
+        x_batch, y_batch = self.__make_tensor()
         idx = 0
         while True:
             my_data = []
-            if idx + num > self.m:
+            if idx + num > x_batch.shape[0]:
                 idx = 0
             start = idx
             idx = idx + num
-            print(idx)
-            data_x = self.x_tensor[start:idx, :]
-            data_y = self.y_tensor[start:idx, :]
+            data_x = x_batch[start:idx, :]
+            data_y = y_batch[start:idx, :]
             my_data.append(data_x)
             my_data.append(data_y)
             yield my_data
+
+    def get_test(self):
+        my_test = []
+        x_test, y_test = self.__make_tensor(tot="test")
+        my_test.append(x_test)
+        my_test.append(y_test)
+        return my_test
 
 
 if __name__ == "__main__":
     mnist = MnistData()
     data = mnist.mini_batch(10)
-    for i in range(5):
-        y = next(data)
-        # print(y)
-        print('---------------')
