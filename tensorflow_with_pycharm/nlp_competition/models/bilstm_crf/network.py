3eb28b4757c83ab230452d664875ad32868d6e55
ChenXi <liucx.sky@gmail.com>
2018-10-28 19:52:51 +0800

    add
    add

--- tensorflow_with_pycharm/nlp_competition/models/bilstm_crf/network.py
+++ tensorflow_with_pycharm/nlp_competition/models/bilstm_crf/network.py
@@ -1,5 +1,8 @@
 import tensorflow as tf
 import numpy as np
+from tensorflow.contrib.rnn import LSTMCell
+from tensorflow.contrib.crf import crf_log_likelihood
+from tensorflow.contrib.crf import viterbi_decode
 
 
 class Settings(object):
@@ -11,12 +14,17 @@ class Settings(object):
         self.layers_num = 2  # 增大的话，会多大程度影响性能
         self.n_classes = 31  # UNK标注成什么？还有不足time step进行补位的项目
         self.vocabulary_size = 3000
+        self.weights_decay = 0.001  # 按照参数传进来
+        self.batch_size = 128
         self.ckpt_path = r'C:\Users\nhn\Documents\GitHub\ml_learn\tensorflow_with_pycharm\nlp_competition\ckpt\\'\
                          + self.model_name + '/'
         self.summary_path = ''
 
 
 class BiLstmCRF(object):
+    """
+    bi_lstm[+dropout] ->flatten->crf
+    """
     def __init__(self, settings):
         self.embedding_size = settings.embedding_size
         self.time_step = settings.time_step
@@ -24,8 +32,11 @@ class BiLstmCRF(object):
         self.layers_num = settings.layers_num
         self.n_classes = settings.n_classes
         self.vocabulary_size = settings.vocabulary_size
+        self._weights_decay = settings.weights_decay
+        self._batch_size = settings.batch_size
+        self._global_steps = tf.Variable(0, trainable=False, name='Global_Step')
 
-        self._keep_prob = tf.placeholder(tf.float32, [])
+        self._dropout_prob = tf.placeholder(tf.float32, [])
         # input placeholder
         with tf.name_scope('Inputs'):
             self._x_inputs = tf.placeholder(tf.int64, [None, self.time_step], name='x_input')
@@ -35,7 +46,23 @@ class BiLstmCRF(object):
                                              initializer=tf.random_uniform_initializer,
                                              dtype=tf.float32, trainable=True, name='embedding')
         with tf.variable_scope('bi_lstm'):
-            self.test_output = self.inference(self.x_inputs)
+            bi_lstm_output = self.inference(self.x_inputs)
+            bi_lstm_output = tf.nn.dropout(bi_lstm_output, self._dropout_prob)
+        with tf.variable_scope('flatten'):
+            flatten_input = tf.reshape(bi_lstm_output, [-1, self.hidden_size * 2])
+            weights = self._variable_with_weight_decay('weights', [self.hidden_size*2, self.n_classes],
+                                                       0.1, self.weights_decay)
+            biases = self._variable_on_cpu('biases', [self.n_classes], tf.constant_initializer(0.1))
+            flatten_out = tf.matmul(flatten_input, weights)+biases
+        with tf.name_scope('crf'):  # 没用variable_scope
+            self.logits = tf.reshape(flatten_out, [-1, self.time_step, self.n_classes])
+            self.sequence_lengths = np.full(self.batch_size, self.time_step, dtype=np.int32)
+            # 需要改data_process，把句子的真实长度保存到npz中，用length做索引
+            log_likelihood, self.transition_params = crf_log_likelihood(
+                inputs=self.logits, tag_indices=self.y_inputs, sequence_lengths=self.sequence_lengths)
+            self.crf_loss = tf.reduce_mean(log_likelihood)
+            self.lost = self.crf_loss + tf.add_n(tf.get_collection('losses'))
+        self.saver = tf.train.Saver(max_to_keep=2)
 
     @property
     def x_inputs(self):
@@ -44,6 +71,22 @@ class BiLstmCRF(object):
     @property
     def y_inputs(self):
         return self._y_inputs
+
+    @property
+    def weights_decay(self):
+        return self._weights_decay
+
+    @property
+    def dropout_prob(self):
+        return self._dropout_prob
+
+    @property
+    def batch_size(self):
+        return self._batch_size
+
+    @property
+    def global_steps(self):
+        return self._global_steps
 
     @staticmethod
     def _variable_on_cpu(name, shape, initializer):
@@ -61,16 +104,21 @@ class BiLstmCRF(object):
 
     def inference(self, x_inputs):
         inputs = tf.nn.embedding_lookup(self.embedding, x_inputs)
-        return inputs
+        cell_fw = LSTMCell(self.hidden_size)
+        cell_bw = LSTMCell(self.hidden_size)
+        (outputs_fw, outputs_bw), _ = tf.nn.bidirectional_dynamic_rnn(
+            cell_fw=cell_fw, cell_bw=cell_bw, inputs=inputs, dtype=tf.float32)
+        outputs = tf.concat([outputs_fw, outputs_bw], axis=-1)
+        return outputs
 
 
 if __name__ == '__main__':
+    my_path = r'C:\Users\Neo\Documents\GitHub\ml_learn\tensorflow_with_pycharm' \
+              r'\nlp_competition\data\process_data\train\0.npz'
+    data = np.load(my_path)
     my_settings = Settings()
     network = BiLstmCRF(my_settings)
-    data = np.load(r'C:\Users\nhn\Desktop\data\process_data\0.npz')
-    # print(data['X'].shape)
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
-        test_result = network.test_output.eval(feed_dict={network.x_inputs: data['X']})
-        print(test_result)
-
+        feed_dict = {network.x_inputs: data['X'], network.y_inputs: data['y'], network.dropout_prob: 1}
+        print(network.lost.eval(feed_dict=feed_dict))
