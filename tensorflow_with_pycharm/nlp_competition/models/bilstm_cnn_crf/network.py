b79a2fe9bb62294c0697ff84bf0185733c31c7a6
ChenXi <liucx.sky@gmail.com>
2018-11-09 16:54:02 +0800

    add
    1. 把历史数据提上去
    2. 增加了cnn获得的embedding

---
+++ tensorflow_with_pycharm/nlp_competition/models/bilstm_cnn_crf/network.py
@@ -0,0 +1,208 @@
+import tensorflow as tf
+import os
+from tensorflow.contrib.crf import crf_log_likelihood
+from tensorflow.contrib.crf import crf_decode
+from tensorflow.contrib import rnn
+from tensorflow.contrib.layers.python.layers import initializers
+
+
+class Settings(object):
+    def __init__(self):
+        self.model_name = 'bi_lstm_cnn_crf'
+        self.embedding_size = 100  # 即使500的话，效果也不好
+        self.cnn_kernel_size = 5
+        self.cnn_kernel_outdim = 50
+        self.cnn_stride = 1
+        self.seq_dim = 40
+        self.hidden_size = 100  # 即使500的话，效果也不好
+        self.layers_num = 1
+
+        self.embed_dropout_prob = 0.5
+        self.weights_decay = 0.001
+        self.time_step = 150
+        self.n_classes = 31
+        self.n_seq = 4
+        self.vocabulary_size = 3000
+        self.root_path = os.path.join(os.path.abspath(os.path.join(os.getcwd(), "../..")))
+        self.ckpt_path = self.root_path + '/ckpt/' + self.model_name + '/'
+        self.summary_path = self.root_path + '/summary/' + self.model_name + '/'
+
+
+class BiLstmCNNCRF(object):
+    """
+    bi_lstm[+dropout] ->flatten[hidden_size*2, hidden_size]->tanh->flatten[hidden_size, n_classes]->crf
+    """
+    def __init__(self, settings):
+        self.embedding_size = settings.embedding_size
+        self.time_step = settings.time_step
+        self.hidden_size = settings.hidden_size
+        self.seq_dim = settings.seq_dim
+        self.layers_num = settings.layers_num
+        self.n_classes = settings.n_classes
+        self.n_seq = settings.n_seq
+        self.vocabulary_size = settings.vocabulary_size
+        self._weights_decay = settings.weights_decay
+        self._embed_dropout_prob = settings.embed_dropout_prob
+        self._cnn_kernel_outdim = settings.cnn_kernel_outdim
+        self._cnn_kernel_size = settings.cnn_kernel_size
+        self._cnn_stride = settings.cnn_stride
+        self._global_steps = tf.Variable(0, trainable=False, name='Global_Step')
+        self.initializer = initializers.xavier_initializer()
+
+        self._dropout_prob = tf.placeholder(tf.float32, [])
+        # input placeholder
+        with tf.name_scope('Inputs'):
+            self._sentence_lengths = tf.placeholder(tf.int32, [None], name='sentence_lengths')
+            self._x_inputs = tf.placeholder(tf.int32, [None, self.time_step], name='x_input')
+            self._y_inputs = tf.placeholder(tf.int32, [None, self.time_step], name='y_input')
+            self._seq_inputs = tf.placeholder(tf.int32, [None, self.time_step], name='seq_input')
+            self._batch_size = tf.placeholder(tf.int32, [], name='batch_size')
+
+        self._embedding = []
+        with tf.variable_scope('embedding'):
+            self._char_embedding = tf.get_variable(shape=[self.vocabulary_size+1, self.embedding_size],
+                                                   initializer=self.initializer,
+                                                   dtype=tf.float32, trainable=True, name='char_embedding')
+            self.char_embedding = tf.nn.embedding_lookup(self._char_embedding, self.x_inputs)
+            self._embedding.append(self.char_embedding)
+            with tf.variable_scope('seq_embedding'):
+                self.seq_embedding = tf.get_variable(shape=[self.n_seq, self.seq_dim],
+                                                     initializer=self.initializer,
+                                                     dtype=tf.float32, trainable=True, name='seq_embedding')
+                self._embedding.append(tf.nn.embedding_lookup(self.seq_embedding, self.seq_inputs))
+            with tf.variable_scope('cnn_embedding'):
+                self.cnn_embedding = self.cnn()
+                self._embedding.append(self.cnn_embedding)
+            self.embedding = tf.concat(self._embedding, axis=-1)
+            self.embedding = tf.nn.dropout(self.embedding, self._embed_dropout_prob)
+        with tf.variable_scope('bi_lstm'):
+            bi_lstm_output = self.inference(self.embedding)
+            bi_lstm_output = tf.nn.dropout(bi_lstm_output, self._dropout_prob)
+        with tf.variable_scope('flatten_middle'):
+            flatten_input = tf.reshape(bi_lstm_output, [-1, self.hidden_size * 2])
+            weights = self._variable_with_weight_decay('weights_middle', [self.hidden_size*2, self.hidden_size],
+                                                       self.weights_decay)
+            tf.summary.histogram('weights_middle', weights)
+            biases = self._variable_on_cpu('biases_middle', [self.hidden_size], tf.zeros_initializer())
+            tf.summary.histogram('biases_middle', biases)
+            _flatten_middle = tf.matmul(flatten_input, weights)+biases
+            flatten_middle = tf.tanh(_flatten_middle)
+        with tf.variable_scope('flatten_out'):
+            weights = self._variable_with_weight_decay('weights_out', [self.hidden_size, self.n_classes],
+                                                       self.weights_decay)
+            tf.summary.histogram('weights_out', weights)
+            biases = self._variable_on_cpu('biases_out', [self.n_classes], tf.zeros_initializer())
+            tf.summary.histogram('biases_out', biases)
+            flatten_out = tf.nn.xw_plus_b(flatten_middle, weights, biases)
+        with tf.name_scope('crf'):  # 没用variable_scope
+            self.logits = tf.reshape(flatten_out, [-1, self.time_step, self.n_classes])
+            self.transition_params = tf.get_variable('transitions',
+                                                     shape=[self.n_classes, self.n_classes],
+                                                     initializer=self.initializer)
+
+            log_likelihood, self.transition_params = crf_log_likelihood(
+                inputs=self.logits, tag_indices=self.y_inputs,
+                transition_params=self.transition_params, sequence_lengths=self.sentence_lengths)
+            self._crf_loss = -tf.reduce_mean(log_likelihood)
+            tf.summary.scalar('crf_lost', self._crf_loss)
+            self.lost = self._crf_loss + tf.add_n(tf.get_collection('losses'))
+            tf.summary.scalar('lost', self.lost)
+
+        with tf.name_scope('predict'):
+            self.predict_sentence, self.best_score = crf_decode(
+                self.logits, self.transition_params, self.sentence_lengths)
+            self._correct_predict = tf.equal(self.predict_sentence, self.y_inputs)
+            self.accuracy = tf.reduce_mean(tf.cast(self._correct_predict, 'float'))
+            tf.summary.scalar('accuracy', self.accuracy)
+            # self.conf_matrix = tf.confusion_matrix(self.y_inputs, self.predict_sentence, num_classes=self.n_classes)
+        self.saver = tf.train.Saver(max_to_keep=2)
+
+    @property
+    def x_inputs(self):
+        return self._x_inputs
+
+    @property
+    def y_inputs(self):
+        return self._y_inputs
+
+    @property
+    def seq_inputs(self):
+        return self._seq_inputs
+
+    @property
+    def weights_decay(self):
+        return self._weights_decay
+
+    @property
+    def dropout_prob(self):
+        return self._dropout_prob
+
+    @property
+    def batch_size(self):
+        return self._batch_size
+
+    @property
+    def global_steps(self):
+        return self._global_steps
+
+    @property
+    def sentence_lengths(self):
+        return self._sentence_lengths
+
+    @staticmethod
+    def _variable_on_cpu(name, shape, initializer):
+        with tf.device('/cpu:0'):
+            var = tf.get_variable(name, shape, initializer=initializer)
+        return var
+
+    def _variable_with_weight_decay(self, name, shape, wb):  # stddv
+        var = self._variable_on_cpu(name, shape, self.initializer)
+
+        if wb:
+            weight_decay = tf.multiply(tf.nn.l2_loss(var), wb)
+            tf.add_to_collection('losses', weight_decay)
+        return var
+
+    def cnn(self):
+        # conv1d input:[batch_size, time_step, embedding_size] output:[batch_size, time_step, 50]
+        cnn_input = tf.nn.dropout(self.char_embedding, self.dropout_prob)
+        weights = self._variable_with_weight_decay(
+            'cnn_weights', [self._cnn_kernel_size, self.embedding_size, self._cnn_kernel_outdim], 0)
+        # kernel[5, hidden, 50]
+        cnn_output = tf.nn.conv1d(cnn_input, weights, self._cnn_stride, padding='SAME', name='conv1d')
+        cnn_output = tf.tanh(cnn_output, name='tanh')
+        # max pooling
+        # cnn_output = tf.layers.max_pooling1d(cnn_output, pool_size=[5], strides=[2], padding='same', name='pool')
+        # dropout
+        cnn_output = tf.nn.dropout(cnn_output, self.dropout_prob)
+        return cnn_output
+
+    def inference(self, inputs):
+        def basic_cell(hidden_size, initializer):
+            _cell = rnn.CoupledInputForgetGateLSTMCell(
+                hidden_size,
+                use_peepholes=True,
+                initializer=initializer)
+            return _cell
+
+        lstm_cell = {}
+        for direction in ['forward', 'backward']:
+            with tf.variable_scope(direction):
+                lstm_cell[direction] = rnn.MultiRNNCell(
+                    [basic_cell(self.hidden_size, self.initializer) for _ in range(self.layers_num)])
+        (outputs_fw, outputs_bw), _ = tf.nn.bidirectional_dynamic_rnn(
+            cell_fw=lstm_cell['forward'], cell_bw=lstm_cell['backward'], inputs=inputs,
+            sequence_length=self.sentence_lengths, dtype=tf.float32)
+        outputs = tf.concat([outputs_fw, outputs_bw], axis=-1)
+        return outputs
+
+
+if __name__ == '__main__':
+    _input = tf.Variable(tf.random_uniform(shape=[10, 128]), dtype=tf.float32)
+    _input = tf.reshape(_input, [10, 128, 1])
+    b = tf.Variable(tf.random_uniform(shape=[6, 1, 100]))
+    conv1d = tf.nn.conv1d(_input, b, 1, padding='SAME')
+    with tf.Session() as sess:
+        tf.global_variables_initializer().run()
+        conv1d = conv1d.eval()
+        print(conv1d.shape)
---
+++ tensorflow_with_pycharm/nlp_competition/models/bilstm_cnn_crf/train.py
@@ -0,0 +1,130 @@
+import tensorflow as tf
+import numpy as np
+import network
+import os
+from tqdm import tqdm
+from tools import get_batch
+
+SETTINGS = network.Settings()
+ROOT_PATH = SETTINGS.root_path
+DATA_TRAIN_PATH = ROOT_PATH + '/data/process_data/train/'
+DATA_VALID_PATH = ROOT_PATH + '/data/process_data/validation/'
+MODEL_PATH = SETTINGS.ckpt_path + 'model.ckpt'
+SUMMARY_PATH = SETTINGS.summary_path
+TRAIN_DROPOUT, NOT_TRAIN_DROPOUT = 0.5, 1.0
+TRAIN_BATCH_SIZE = 200
+N_TR_BATCHES, N_VA_BATCHES = len(os.listdir(DATA_TRAIN_PATH)), len(os.listdir(DATA_VALID_PATH))
+
+LEARNING_RATE = 1e-3
+LR_DECAY_RATE = 0.60
+DECAY_STEP = 145
+LAST_ACCURACY = 0.90
+CLIP = 5
+MAX_EPOCH = 20
+VALID_STEP = 100
+
+
+def get_feed_dict(model, get_batch_return, batch_size, feed_control):
+    """
+    :param model: network
+    :param get_batch_return: train or valid --> x, y, sentence_length, inword : 4
+                                     submit --> x, len, belong, comma, inword : 5
+    :param feed_control: is_train, is_valid, is_submit
+    :param batch_size: train[valid]batch_size or submit batch_size
+    :return: feed_dict
+    """
+    data_list = list(get_batch_return)
+    feed_dict = {model.x_inputs: data_list[0], model.seq_inputs: data_list[1], model.sentence_lengths: data_list[2],
+                 model.batch_size: batch_size}
+    if feed_control in ['is_train', 'is_valid']:
+        feed_dict[model.y_inputs] = data_list[3]
+        feed_dict[model.dropout_prob] = TRAIN_DROPOUT
+        if feed_control == 'is_valid':
+            feed_dict[model.dropout_prob] = NOT_TRAIN_DROPOUT
+    elif feed_control == 'is_submit':
+        feed_dict[model.dropout_prob] = NOT_TRAIN_DROPOUT
+    else:
+        raise ValueError('no control type:', feed_control)
+    return feed_dict
+
+
+def valid_epoch(path, model, sess):
+    _accuracy_list = list()
+    for batch_id in range(N_VA_BATCHES):
+        feed_dict = get_feed_dict(model, get_batch(path, batch_id), TRAIN_BATCH_SIZE, feed_control='is_valid')
+        accuracy = sess.run(model.accuracy, feed_dict)
+        _accuracy_list.append(accuracy)
+    mean_accuracy = sess.run(tf.reduce_mean(_accuracy_list))
+    return mean_accuracy
+
+
+def train_epoch(valid_path, model, sess, train_fetches, valid_fetches, train_writer, test_writer):
+    global LEARNING_RATE
+    global LAST_ACCURACY
+
+    random_batch = np.random.permutation(N_TR_BATCHES)
+    for batch in tqdm(range(N_TR_BATCHES)):
+        global_steps = sess.run(model.global_steps)
+        if (global_steps+1) % VALID_STEP == 0:
+            mean_accuracy = valid_epoch(valid_path, model, sess)
+            if mean_accuracy > LAST_ACCURACY:
+                LAST_ACCURACY = mean_accuracy
+                saving_path = model.saver.save(sess, MODEL_PATH, global_steps+1)
+                print('saved new model to %s ' % saving_path)
+            print('the step is %d and the validation mean accuracy is %g' % (global_steps + 1, mean_accuracy))
+
+        batch_id = random_batch[batch]
+        feed_dict = get_feed_dict(model, get_batch(DATA_TRAIN_PATH, batch_id),
+                                  TRAIN_BATCH_SIZE, feed_control='is_train')
+        summary, _ = sess.run(train_fetches, feed_dict)
+
+        if (global_steps+1) % 20 == 0:
+            train_writer.add_summary(summary, global_steps)
+            summary, lost = sess.run(valid_fetches, feed_dict)
+            test_writer.add_summary(summary, global_steps)
+            print('the step is %d and the train data set lost is %g' % (global_steps+1, lost))
+
+
+def main():
+    config = tf.ConfigProto(
+        device_count={'CPU': 4},
+        intra_op_parallelism_threads=4,
+        inter_op_parallelism_threads=4)
+    config.gpu_options.allow_growth = True
+
+    with tf.Session(config=config) as sess:
+        model = network.BiLstmCNNCRF(SETTINGS)
+        with tf.variable_scope('training_op') as vs:
+            learning_rate = tf.train.exponential_decay(LEARNING_RATE, model.global_steps,
+                                                       DECAY_STEP, LR_DECAY_RATE, staircase=True)
+
+            optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+            grads_and_vars = optimizer.compute_gradients(model.lost)
+            grads_and_vars_clips = [[tf.clip_by_value(g, -CLIP, CLIP), v] for g, v in grads_and_vars]
+            train_op = optimizer.apply_gradients(grads_and_vars_clips, global_step=model.global_steps)
+
+            merged = tf.summary.merge_all()
+            train_writer = tf.summary.FileWriter(SUMMARY_PATH + 'train', sess.graph)
+            test_writer = tf.summary.FileWriter(SUMMARY_PATH + 'test')
+            training_ops = [v for v in tf.global_variables() if v.name.startswith(vs.name+'/')]
+
+        if os.path.exists(SETTINGS.ckpt_path + 'checkpoint'):
+            print('Restoring Variables from Checkpoint...')
+            model.saver.restore(sess, tf.train.latest_checkpoint(SETTINGS.ckpt_path))
+            mean_accuracy = valid_epoch(DATA_VALID_PATH, model, sess)
+            print('valid mean accuracy is %g' % mean_accuracy)
+            sess.run(tf.variables_initializer(training_ops))
+        else:
+            print('Initializing Variables...')
+            tf.global_variables_initializer().run()
+
+        for epoch in range(MAX_EPOCH):
+            global_step = sess.run(model.global_steps)
+            print('the step is %d, and the learning rate is %g' % (global_step, sess.run(learning_rate)))
+            train_fetches = [merged, train_op]
+            valid_fetches = [merged, model.lost]
+            train_epoch(DATA_VALID_PATH, model, sess, train_fetches, valid_fetches, train_writer, test_writer)
+
+
+if __name__ == '__main__':
+    main()
